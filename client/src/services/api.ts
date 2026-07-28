a1ce2bb54fa042bda0c868a2c8bd1d92882d0bf2
Georgy <ggsurkov@gmail.com>
2026-07-29 00:23:58 +0200

    feat: implement axios instance with JWT interceptors and automatic session handling

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: '/api',
 });
 
 api.interceptors.request.use(config => {
